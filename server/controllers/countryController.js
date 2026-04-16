87a61a43255608a46438c0d9ef3aa970b33dffaa
Violane-7 <smeraldo223@gmail.com>
2026-04-16 13:45:36 +0530

    finally backend working

--- server/controllers/countryController.js
+++ server/controllers/countryController.js
@@ -1,11 +1,19 @@
-import data from "../data/countries.json" assert { type: "json" };
+import fs from "fs";
+
+// Read JSON file safely
+const rawData = fs.readFileSync(
+  new URL("../data/countries.json", import.meta.url)
+);
+
+const data = JSON.parse(rawData);
 
 export const getCountry = (req, res) => {
   const code = req.params.code.toUpperCase();
+
   const country = data[code];
 
   if (!country) {
-    return res.status(404).json({ message: "Not found" });
+    return res.status(404).json({ message: "Country not found" });
   }
 
   res.json(country);
